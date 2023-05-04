e9011eec5575f031fc31599cc1f9f7b0654cbe88
harsh-android <harshche123@gmail.com>
2023-05-04 15:20:19 +0530

    Add Play Command Respond

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
+import datetime
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices  =engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-engine.say("Hello, I am Harsh")
-engine.runAndWait()
 
-try:
-    with sr.Microphone() as source:
-        print('listning')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if 'alexa' in command:
-            print(command)
-            
-except:
-    pass
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listning')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+            elif 'time' in command:
+                time = datetime.datetime.now().strftime('%I:%M %p')
+                print(time)
+                talk('Current time is ' + time)
+            elif 'are you single' in command:
+                talk('No, I am Relationship with Harsh') 
+                
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command1 = take_command()
+    if 'play' in command1:
+        song = command1.replace('play', '')
+        talk('playing some Songs')
+        pywhatkit.playonyt(song)
+        
+run_alexa()
+    
